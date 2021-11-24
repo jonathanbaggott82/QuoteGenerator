688da7a4b6c37b821037cad0087eb30424b8a45d
jonathanbaggott82 <jonathan.baggott82@gmail.com>
2021-11-24 17:18:36 +0000

    added the loading graphic

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -21,13 +36,15 @@ async function getQuote() {
             authorText.innerText = data.quoteAuthor;
         }
         // Reduce font size for long quotes
-        if (data.quoteText.length > 10) {
+        if (data.quoteText.length > 100) {
             quoteText.classList.add('long-quote');
         }
         else {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
     } 
     catch (error) {
     }
